82a44b0a012939150d504e7f6a8195dbb8fbceeb
jinkuen <taiwan.badboy99@gmail.com>
2011-02-02 01:23:22 +0800

    add .pdc writer, add comment about .pdc reader

--- src/lib/io/PDC.cpp
+++ src/lib/io/PDC.cpp
@@ -6,12 +6,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-//#include <cassert>
 #include <memory>
 
 namespace Partio{
 
 using namespace std;
+
+static const int PDC_MAGIC = (((((' '<<8)|'C')<<8)|'D')<<8)|'P'; // " CDP"
 
 typedef struct{
     int magic;
@@ -33,7 +34,6 @@ string readName(istream& input){
     return result;
 }
 
-static const int MC_MAGIC = (((((' '<<8)|'C')<<8)|'D')<<8)|'P'; // " CDP"
 ParticlesDataMutable* readPDC(const char* filename, const bool headersOnly){
 
     auto_ptr<istream> input(Gzip_In(filename,std::ios::in|std::ios::binary));
@@ -44,56 +44,41 @@ ParticlesDataMutable* readPDC(const char* filename, const bool headersOnly){
 
     PDC_HEADER header;
     input->read((char*)&header, sizeof(header)); 
-    if(MC_MAGIC != header.magic){
-        std::cerr << "Partio: Magic number '" << header.magic << "' of '" << filename << "' doesn't match mc magic '" << MC_MAGIC << "'" << std::endl;
+    if(PDC_MAGIC != header.magic){
+        std::cerr << "Partio: Magic number '" << header.magic << "' of '" << filename << "' doesn't match pdc magic '" << PDC_MAGIC << "'" << std::endl;
         return 0;
     }
 
     BIGEND::swap(header.numParticles);
     BIGEND::swap(header.numAttrs);
 
-    ParticlesDataMutable* simple=0;
-    if(headersOnly){
-        simple = new ParticleHeaders;
-    } 
-    else{
-        simple = create();
-    }
-
+    ParticlesDataMutable* simple = headersOnly ? new ParticleHeaders: create();
     simple->addParticles(header.numParticles);
 
-    for(int i = 0; i < header.numAttrs; i++){
+    for(int attrIndex = 0; attrIndex < header.numAttrs; attrIndex++){
+        // add attribute
+        ParticleAttribute attr;
         string attrName = readName(*input);
         int type;
-        read<BIGEND>(*input, type);
+        read<BIGEND>(*input, type);     
         if(type == 3){
-            ParticleAttribute attrHandle = simple->addAttribute(attrName.c_str(), FLOAT, 1);
-            if(headersOnly){
-                input->seekg((int)input->tellg() + header.numParticles*sizeof(double));
-                continue;
-            }
-            else{
-                double tmp;
-                for(int j = 0; j < simple->numParticles(); j++){
-                    read<BIGEND>(*input, tmp);
-                    simple->dataWrite<float>(attrHandle, j)[0] = tmp;
-                }
-            }
+            attr = simple->addAttribute(attrName.c_str(), FLOAT, 1);
         }
         else if(type == 5){
-            ParticleAttribute attrHandle = simple->addAttribute(attrName.c_str(), VECTOR, 3);
-            if(headersOnly){
-                input->seekg((int)input->tellg() + header.numParticles*sizeof(double)*attrHandle.count);
-                continue;
-            }
-            else{
-                for(int j = 0; j < simple->numParticles(); j++){
-                    double tmp[3];
-                    read<BIGEND>(*input, tmp[0], tmp[1], tmp[2]);
-                    float* data = simple->dataWrite<float>(attrHandle, j);
-                    data[0] = tmp[0];
-                    data[1] = tmp[1];
-                    data[2] = tmp[2];
+            attr = simple->addAttribute(attrName.c_str(), VECTOR, 3);
+        }
+
+        // if headersOnly, skip 
+        if(headersOnly){
+            input->seekg((int)input->tellg() + header.numParticles*sizeof(double)*attr.count);
+            continue;
+        }
+        else{
+            double tmp[3];
+            for(int partIndex = 0; partIndex < simple->numParticles(); partIndex++){
+                for(int dim = 0; dim < attr.count; dim++){
+                    read<BIGEND>(*input, tmp[dim]);
+                    simple->dataWrite<float>(attr, partIndex)[dim] = tmp[dim];
                 }
             }
         }
@@ -103,7 +88,49 @@ ParticlesDataMutable* readPDC(const char* filename, const bool headersOnly){
 }
 
 bool writePDC(const char* filename,const ParticlesData& p,const bool compressed){
-    return 0;
+    auto_ptr<ostream> output(
+        compressed ?
+        Gzip_Out(filename,ios::out|ios::binary)
+        :new std::ofstream(filename,ios::out|ios::binary));
+
+    if(!*output){
+        cerr << "Partio Unable to open file " << filename << endl;
+        return false;
+    }
+ 
+    // write .pdc header
+    write<LITEND>(*output, PDC_MAGIC);
+    write<BIGEND>(*output, (int)1); // version
+    write<BIGEND>(*output, (int)1); // bitorder
+    write<BIGEND>(*output, (int)0); // tmp1
+    write<BIGEND>(*output, (int)0); // tmp2
+    write<BIGEND>(*output, (int)p.numParticles());
+    write<BIGEND>(*output, (int)p.numAttributes());
+
+    for(int attrIndex = 0; attrIndex < p.numAttributes(); attrIndex++){
+        ParticleAttribute attr;
+        p.attributeInfo(attrIndex,attr);
+
+        // write attribute name
+        write<BIGEND>(*output, (int)attr.name.length());
+        output->write(attr.name.c_str(), (int)attr.name.length());
+
+        // write type
+        int count = 1; // FLOAT
+        if(attr.type == VECTOR){
+            count = 3;
+        }
+        write<BIGEND>(*output, (int)(count+2));
+
+        // write data
+        for(int partIndex = 0; partIndex < p.numParticles(); partIndex++){
+            const float* data = p.data<float>(attr, partIndex);
+            for(int dim = 0; dim < count; dim++){
+                write<BIGEND>(*output, (double)data[dim]);
+            }
+        }
+    }
+    return true;
 }
 
 }// end of namespace Partio
